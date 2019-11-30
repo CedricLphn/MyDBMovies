c8b5d651783be0902a5dc12eb227ba7c9222e40d
Leprohon <cedric.leprohon@yahoo.fr>
2019-11-30 22:55:17 +0100

    fix

--- src/views/ProfileScreen.js
+++ src/views/ProfileScreen.js
@@ -2,42 +2,37 @@ import React, { useState, useEffect} from 'react';
 import { SafeAreaView, FlatList } from 'react-native';
 import {useSelector} from 'react-redux';
 import Favorite from '../components/Favorite';
-import {
-  getRowFromFirestore,
-  addtoFirestore,
-  deleteFromFirestore,
-  getDataFromFirestore
-} from '../helpers/vendors/Firebase'
 
 
 const ProfileScreen = () => {
   const darkMode = useSelector(state => state.darkModeReducer);
 
 
-  const state = useSelector(state => state.favoritesListReducer)
-  const user = useSelector(state => state.usersReducer).username
+  const state = useSelector(state => state.favoritesListReducer).favorites
+  console.log("state favorite", state);
+  //const user = useSelector(state => state.usersReducer).username
 
-  const [favList, setFavList] = useState(false)
+  //const [favList, setFavList] = useState(false)
 
   // loading
-  useEffect(() => {
-    getAllFavorites()
-  })
+  // useEffect(() => {
+  //   getAllFavorites()
+  // })
 
-  // get the list of favorites from firestore
-  const getAllFavorites = async() => {
-    const tmp = await getDataFromFirestore(user)
-    if(tmp.length>0)
-      setFavList(tmp)
-  }
+  // // get the list of favorites from firestore
+  // const getAllFavorites = async() => {
+  //   const tmp = await getDataFromFirestore(user)
+  //   if(tmp.length>0)
+  //     setFavList(tmp)
+  // }
 
   // generate the flatList of favorites
   function listOfFavorites() {
     return (
         <FlatList
-            data={favList}
+            data={state}
             renderItem={({item}) => <Favorite item={item}/> }
-            keyExtractor={item => item.idFromApi.toString()}
+            keyExtractor={item => item.idFromApi}
         />
     )
   }
